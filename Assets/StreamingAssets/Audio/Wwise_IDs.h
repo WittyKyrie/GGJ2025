9a841eb485aab92751605913ab53a26132fc9297
Jerry Tang <jerrytang.c@gmail.com>
2025-01-19 14:11:23 +0800

    Revert "音效补充"
    This reverts commit 3cffca55c79113d8e4991b1dead3f740f5b4f282.

--- Assets/StreamingAssets/Audio/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/Wwise_IDs.h
@@ -16,30 +16,9 @@ namespace AK
         static const AkUniqueID PLAY_BGM_INGAME = 2167428848U;
         static const AkUniqueID PLAY_SFX_AMB_BAR_INGAME = 3282807380U;
         static const AkUniqueID PLAY_SFX_AMB_BAR_MENU = 2571413926U;
-        static const AkUniqueID PLAY_SFX_CLICK_NORMAL = 604102624U;
         static const AkUniqueID PLAY_SFX_CLICK_START = 1695911055U;
-        static const AkUniqueID PLAY_SFX_CUT_SENCE = 495583777U;
-        static const AkUniqueID PLAY_SFX_DRINKING_ROUND = 3700857917U;
         static const AkUniqueID PLAY_SFX_POURING_BEER = 2726246453U;
-        static const AkUniqueID PLAY_SFX_ROUND_NOTICE = 1032364325U;
-        static const AkUniqueID STOP_SFX_POURING_BEER = 4200562683U;
     } // namespace EVENTS
-
-    namespace STATES
-    {
-        namespace BEER_POURING
-        {
-            static const AkUniqueID GROUP = 4038591736U;
-
-            namespace STATE
-            {
-                static const AkUniqueID NO = 1668749452U;
-                static const AkUniqueID NONE = 748895195U;
-                static const AkUniqueID YES = 979470758U;
-            } // namespace STATE
-        } // namespace BEER_POURING
-
-    } // namespace STATES
 
     namespace BANKS
     {
